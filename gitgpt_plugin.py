e391ed76b9cf6d316480a0f92c636900e3cccfee
pliny <133052465+elder-plinius@users.noreply.github.com>
2023-09-04 10:00:55 -0700

    Update gitgpt_plugin.py

--- gitgpt_plugin.py
+++ gitgpt_plugin.py
@@ -94,4 +94,4 @@ def push_code():
         return jsonify({"message": "Failed to push code", "error": response.json()}), response.status_code
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0', port=3000, debug=True)
